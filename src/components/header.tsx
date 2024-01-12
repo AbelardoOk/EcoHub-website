62300d37184342a0fa5b3bb46161fe988b309853
AbelardoOk <abelardoemailprivado@gmail.com>
2024-01-12 15:04:12 -0400

    feat: Melhorias feitas na header do site: novas animações

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,9 @@ import { CartIcon } from "../../public/cart-shopping";
 import { EnterIcon } from "../../public/right-to-bracket";
 import { PhoneIcon } from "../../public/phone";
 import { LogoIcon } from "../../public/logo";
+
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export function Header() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -21,72 +23,57 @@ export function Header() {
   };
 
   return (
-    <header
+    <motion.header
       className="fixed flex h-screen flex-col justify-between bg-paleta-verde px-1 py-12"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      initial={{ width: "3.5rem" }} // Largura inicial quando não expandido
+      animate={{ width: isExpanded ? "10rem" : "3.5rem" }} // Largura durante a transição
+      transition={{ duration: 0.3 }} // Duração da transição
     >
       <div className="flex flex-col align-middle font-sans font-semibold text-paleta-marrom">
-        <a href="#home" className="flex flex-row gap-2 p-2">
+        <a
+          href="#home"
+          className="flex flex-row gap-2 p-2 transition-all ease-in-out hover:text-paleta-bege"
+        >
           <HouseIcon />
-          {isExpanded && (
-            <>
-              <p className="">Menu</p>
-            </>
-          )}
+          {isExpanded && <p className="">Menu</p>}
         </a>
-        <a href="#dicas" className="flex flex-row gap-2 p-2">
+        <a
+          href="#dicas"
+          className="flex flex-row gap-2 p-2  transition-all ease-in-out hover:text-paleta-bege"
+        >
           <LeafIcon />
-          {isExpanded && (
-            <>
-              <p className="">Dicas</p>
-            </>
-          )}
+          {isExpanded && <p className="">Dicas</p>}
         </a>
-        <a href="#comunidade" className="flex flex-row gap-2 p-2">
+        <a
+          href="#comunidade"
+          className="flex flex-row gap-2 p-2  transition-all ease-in-out hover:text-paleta-bege"
+        >
           <UsersIcon altura={24} largura={24} />
-          {isExpanded && (
-            <>
-              <p className="">Comunidades</p>
-            </>
-          )}
+          {isExpanded && <p className="">Comunidades</p>}
         </a>
-        <a className="flex flex-row gap-2 p-2">
+        <a className="flex flex-row gap-2 p-2  transition-all ease-in-out hover:text-paleta-bege">
           <CalendarIcon />
-          {isExpanded && (
-            <>
-              <p className="">Calendario</p>
-            </>
-          )}
+          {isExpanded && <p className="">Calendario</p>}
         </a>
-        <a className="flex flex-row gap-2 p-2">
+        <a className="flex flex-row gap-2 p-2  transition-all ease-in-out hover:text-paleta-bege">
           <CartIcon />
-          {isExpanded && (
-            <>
-              <p className="">Carrinho</p>
-            </>
-          )}
+          {isExpanded && <p className="">Carrinho</p>}
         </a>
-        <a className="flex flex-row gap-2 p-2">
+        <a className="flex flex-row gap-2 p-2 transition-all ease-in-out hover:text-paleta-bege">
           <EnterIcon />
-          {isExpanded && (
-            <>
-              <p className="">Login</p>
-            </>
-          )}
+          {isExpanded && <p className="">Login</p>}
         </a>
-        <a className="flex flex-row gap-2 p-2">
+        <a className="flex flex-row gap-2 p-2 transition-all ease-in-out hover:text-paleta-bege">
           <PhoneIcon />
-          {isExpanded && (
-            <>
-              <p className="">Contato</p>
-            </>
-          )}
+          {isExpanded && <p className="">Contato</p>}
         </a>
       </div>
-      <div className="p-2">
+      <div className="flex flex-row gap-2 p-2">
         <LogoIcon altura={24} largura={24} />
+        {isExpanded && <p className=" font-alt">Ecohub</p>}
       </div>
-    </header>
+    </motion.header>
   );
 }
